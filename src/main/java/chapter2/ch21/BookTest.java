e58251c298a40926d9158b431dd1b9d947cc3adc
yunseung <yseungun.21@gmail.com>
2024-02-10 23:48:24 +0900

    feat: 객체 복사 (같은 주소 참조)

--- src/main/java/chapter2/ch21/BookTest.java
+++ src/main/java/chapter2/ch21/BookTest.java
@@ -4,6 +4,7 @@ public class BookTest {
 
     public static void main(String[] args) {
         Book[] library = new Book[3];
+        Book[] copyLibrary = new Book[3];
 
         library[0] = new Book("해리포터", "J.K 롤링");
         library[1] = new Book("홍길동전", "허균");
@@ -11,6 +12,26 @@ public class BookTest {
 
         for(Book book : library) {
             System.out.println(book);
+            book.showInfo();
+        }
+
+        System.arraycopy(library, 0, copyLibrary, 0, 3);
+
+        System.out.println("======copy library=========");
+        for(Book book : copyLibrary) {
+            book.showInfo();
+        }
+
+        library[0].setTitle("Harry Potter");
+        library[0].setAuthor("J.K Rowling");
+
+        System.out.println("======origin library=========");
+        for(Book book : library) {
+            book.showInfo();
+        }
+
+        System.out.println("======copy library=========");
+        for(Book book : copyLibrary) {
             book.showInfo();
         }
     }
