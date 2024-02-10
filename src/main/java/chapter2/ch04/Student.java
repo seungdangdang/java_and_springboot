21a79a5bab7b89294f87e4f583de15b530162552
yunseung <yseungun.21@gmail.com>
2024-02-10 14:47:59 +0900

    feat: student 생성자 적용

--- src/main/java/chapter2/ch04/Student.java
+++ src/main/java/chapter2/ch04/Student.java
@@ -2,10 +2,15 @@ package chapter2.ch04;
 
 public class Student {
 
-    public String studentName;
     public int studentId;
+    public String studentName;
     public String studentAddress;
 
+    public Student(int studentId, String studentName, String studentAddress) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.studentAddress = studentAddress;
+    }
     public void showStudentInfo() {
         System.out.println(studentName + "의 학번은 " + studentId + "이고, 사는 곳은 " + studentAddress + "이다.");
     }
--- src/main/java/chapter2/ch04/StudentTest.java
+++ src/main/java/chapter2/ch04/StudentTest.java
@@ -4,10 +4,7 @@ public class StudentTest {
 
     public static void main(String[] args) {
 
-        Student studentHong = new Student();
-        studentHong.studentName = "홍길동";
-        studentHong.studentId = 1;
-        studentHong.studentAddress = "서울시 강남구";
+        Student studentHong = new Student(1, "홍길동", "서울시 강남구");
 
         studentHong.showStudentInfo();
     }
