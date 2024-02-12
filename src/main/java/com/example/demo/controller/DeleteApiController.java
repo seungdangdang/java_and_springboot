282334c898e315261fb001208420afba235fa2b4
yunseung <yseungun.21@gmail.com>
2024-02-12 22:45:57 +0900

    feat: delete API 작성

---
+++ src/main/java/com/example/demo/controller/DeleteApiController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class DeleteApiController {
+
+    @DeleteMapping("/delete/{userId}")
+    public void delete(@PathVariable String userId, @RequestParam String account) {
+        System.out.println(userId);
+        System.out.println(account);
+
+        // 삭제하기 전에 값이 없어도 코드200을 던짐 = 멱등성
+    }
+}
