3552bd123910a24ed07232cd865d8addf1d9c1b7
yunseung <yseungun.21@gmail.com>
2024-02-12 22:48:24 +0900

    chore: 주석 수정

--- src/main/java/com/example/demo/controller/DeleteApiController.java
+++ src/main/java/com/example/demo/controller/DeleteApiController.java
@@ -16,5 +16,6 @@ public class DeleteApiController {
         System.out.println(account);
 
         // 삭제하기 전에 값이 없어도 코드200을 던짐 = 멱등성
+        // delete는 요청하는 값이 제한적이고 적기 때문에 따로 dto를 만들어서 받기보다는 pathVariable 형태 또는 queryParameter를 받는 것을 권장함
     }
 }
